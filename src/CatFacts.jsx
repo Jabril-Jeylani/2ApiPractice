26c8f8a004a16bb04d7c1280127df86b686c1800
Jabril-Jeylani <jabrilmj@gmail.com>
2023-07-24 15:27:33 -0600

    Implemented 3 different pictures

--- src/CatFacts.jsx
+++ src/CatFacts.jsx
@@ -3,10 +3,13 @@ import { useEffect, useState } from "react"
 function CatFacts() {
 
     const [data, setData] = useState({})
-    const [cat, setCat] = useState('')
+    const [cat, setCat] = useState({
+
+    })
     
     useEffect(() => {fetchData1() 
-        fetchData2()}, [])
+        fetchData2()
+        randomCatTag()}, [])
 
     async function fetchData1() {
         let url = "https://meowfacts.herokuapp.com/?count=3"
@@ -18,23 +21,29 @@ function CatFacts() {
     }
 
     async function fetchData2() {
-        let url = "https://cataas.com/cat"
+        let url = "https://cataas.com/api/tags"
 
         const response = await fetch(url)
-        // const data = await response.json()
-        console.log(response)
+        const data = await response.json()
+        console.log(data)
         setCat(data)
     }
 
-    
+    function randomCatTag() {
+
+        let randomCat = Math.floor(Math.random() * 595)
+        
+        return randomCat
+        
+    }
 
     return (
     <div>
-        <img src={"https://cataas.com/cat"} />
+        <img src={`https://cataas.com/cat/${cat[randomCatTag()]}`} />
         <h2>{data.data ? data.data[0] : 'loading'}</h2>
-        <img src={"https://cataas.com/cat"} />
+        <img src={`https://cataas.com/cat/${cat[randomCatTag()]}`} />
         <h2>{data.data ? data.data[1] : 'loading'}</h2>
-        <img src={"https://cataas.com/cat"} />
+        <img src={`https://cataas.com/cat/${cat[randomCatTag()]}`} />
         <h2>{data.data ? data.data[2] : 'loading'}</h2>
     </div>
     )
